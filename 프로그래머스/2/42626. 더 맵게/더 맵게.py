af9b8bad7dfc7abd53d3ba362f5d8f7797fa66da
jissssu <113964353+jissssu@users.noreply.github.com>
2024-08-13 06:11:27 +0900

    [level 2] Title: 더 맵게, Time: 1628.89 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -4,11 +4,15 @@ def solution(scoville, k):
     answer = 0
     heapq.heapify(scoville)
 
-    while scoville[0] < k:
-        if len(scoville) == 1: return -1
-
-        heapq.heappush(scoville, heapq.heappop(scoville) + heapq.heappop(scoville) * 2)
+    while len(scoville) > 1 and scoville[0] < k:  # 최소 두 개의 요소가 있는지 확인
+        first = heapq.heappop(scoville)
+        second = heapq.heappop(scoville)
+        new_scoville = first + (second * 2)
+        heapq.heappush(scoville, new_scoville)
         answer += 1
 
+    # 만약 힙에서 가장 작은 값이 K 이상이 아니면 -1을 반환
+    if scoville[0] < k:
+        return -1
 
     return answer
