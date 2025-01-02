6f4f9010decc8eb9c6909bfaa31c19f03d56c3a1
jissssu <113964353+jissssu@users.noreply.github.com>
2025-01-02 23:26:32 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -0,0 +1,14 @@
+def solution(a, b):
+    answer = 0
+    
+    tmp1 = int(str(a) + str(b))
+    tmp2 = int(str(b) + str(a))
+    
+    if tmp1 > tmp2:
+        answer = tmp1
+    elif tmp1 < tmp2:
+        answer = tmp2
+    else:
+        answer = tmp1
+    
+    return int(answer)
