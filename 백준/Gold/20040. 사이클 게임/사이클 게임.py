77ec1e4073a2eb8b5a966fd24c584768e1fd935e
jissssu <113964353+jissssu@users.noreply.github.com>
2025-02-23 20:25:34 +0900

    [Gold IV] Title: 사이클 게임, Time: 1080 ms, Memory: 56772 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.py
@@ -0,0 +1,42 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def find(x):
+    if x != parent[x]:
+        parent[x] = find(parent[x])  
+    return parent[x]
+
+
+def union_(a, b):
+    a = find(a)
+    b = find(b)
+
+    if a == b:
+        return
+
+    if rank[a] > rank[b]:
+        parent[b] = a 
+    elif rank[a] < rank[b]:  
+        parent[a] = b 
+    else:  
+        rank[b] += 1 
+        parent[a] = b 
+
+
+N, M = map(int, input().split())
+
+parent = [i for i in range(N)]
+rank = [0] * N 
+ans = 0
+for i in range(M):
+    a, b = map(int, input().split())
+
+    if not ans:  
+        if find(a) == find(b):  
+            ans = i + 1 
+        else:  
+            union_(a, b)  
+
+print(ans)
