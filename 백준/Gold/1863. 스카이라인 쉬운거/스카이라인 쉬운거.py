d479fbeaab0fffc13c95349f51cd45240ac08d23
jissssu <113964353+jissssu@users.noreply.github.com>
2025-02-27 19:41:51 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 120 ms, Memory: 110964 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.py
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.py
@@ -6,17 +6,17 @@ for _ in range(n):
     skyline.append(y)
 
 stack = []
-count = 0 
+count = 0  
 
 for height in skyline:
     while stack and stack[-1] > height:
-        stack.pop()  
+        stack.pop()
 
     if stack and stack[-1] == height:
         continue  
 
     if height > 0:
-        stack.append(height)  
-        count += 1 
+        stack.append(height)
+        count += 1  
 
 print(count)
