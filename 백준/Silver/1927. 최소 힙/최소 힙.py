eccbc6e19ad1476098401c58a7a40a5b30d585ef
jissssu <113964353+jissssu@users.noreply.github.com>
2025-03-05 00:24:25 +0900

    [Silver II] Title: 최소 힙, Time: 156 ms, Memory: 114208 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.py
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -3,19 +3,15 @@ import sys
 
 input = sys.stdin.readline
 
-def process(n, nums):
-    heap = []
+n = int(input().strip())
+heap = []
 
-    for num in nums:
-        if num > 0:
-            heapq.heappush(heap, num)
+for _ in range(n):
+    num = int(input().strip())
+    if num > 0:
+        heapq.heappush(heap, num)
+    else:
+        if heap:
+            print(heapq.heappop(heap))
         else:
-            if heap:
-                print(heapq.heappop(heap))
-            else:
-                print(0)
-
-if __name__ == '__main__':
-    n = int(input())
-    nums = [int(input()) for _ in range(n)]
-    process(n, nums)
+            print(0)
