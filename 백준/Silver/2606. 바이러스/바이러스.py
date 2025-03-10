1fd13aa3b178e674be05c8ed603395041ddee02b
jissssu <113964353+jissssu@users.noreply.github.com>
2025-03-10 18:51:26 +0900

    [Silver III] Title: 바이러스, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,31 +1,41 @@
-# 컴퓨터 수 입력
+# 컴퓨터의 수 입력받기
 n = int(input())
-# 연결된 컴퓨터 쌍의 수 입력
+# 연결된 컴퓨터 쌍의 수 입력받기
 m = int(input())
 
-# 그래프 초기화
-graph = [[] for _ in range(n+1)]
+# 각 컴퓨터와 연결된 컴퓨터들을 저장할 리스트
+# 0번 인덱스는 사용하지 않고, 1번부터 n번까지 사용
+connections = [[] for _ in range(n+1)]
 
-# 그래프 정보 입력
+# 연결 정보 입력받기
 for _ in range(m):
     a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)  # 양방향 연결
+    # a와 b가 연결되어 있다는 정보 저장 (양방향)
+    connections[a].append(b)
+    connections[b].append(a)
 
-# 방문 여부를 체크하는 리스트
-visited = [False] * (n+1)
+# 각 컴퓨터의 감염 여부를 저장할 리스트
+infected = [False] * (n+1)
 
 # DFS 함수 정의
-def dfs(start):
-    visited[start] = True
-    for i in graph[start]:
-        if not visited[i]:
-            dfs(i)
+def dfs(computer):
+    # 현재 컴퓨터를 감염 처리
+    infected[computer] = True
+    
+    # 현재 컴퓨터와 연결된 모든 컴퓨터에 대해
+    for connected_computer in connections[computer]:
+        # 아직 감염되지 않았다면 감염시키기
+        if not infected[connected_computer]:
+            dfs(connected_computer)
 
-# 1번 컴퓨터에서 시작
+# 1번 컴퓨터부터 바이러스 퍼뜨리기 시작
 dfs(1)
 
-# 1번 컴퓨터를 제외하고 감염된 컴퓨터 수 계산
-infected = sum(visited) - 1  # 1번 컴퓨터 자신은 제외
+# 1번 컴퓨터를 제외하고 감염된 컴퓨터의 수 세기
+# infected[1]은 항상 True이므로 1번 컴퓨터는 카운트에서 제외
+count = 0
+for i in range(2, n+1):  # 2번 컴퓨터부터 n번 컴퓨터까지
+    if infected[i]:
+        count += 1
 
-print(infected)
+print(count)
