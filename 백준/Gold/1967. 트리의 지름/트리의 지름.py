b6a0c6a71ad016cf370a91b4b669813e52e452d6
jissssu <113964353+jissssu@users.noreply.github.com>
2025-03-21 08:59:50 +0900

    [Gold IV] Title: 트리의 지름, Time: 112 ms, Memory: 239296 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -1,21 +1,63 @@
 import sys
-sys.setrecursionlimit(100000)
-n = int(sys.stdin.readline())
+sys.setrecursionlimit(100000)  # 재귀 제한을 늘려줘요
+
+input = sys.stdin.readline  # 입력을 빠르게 받기 위해 변경했어요
+n = int(input())  # 노드의 개수를 입력받아요
+
+# 트리를 저장할 리스트를 만들어요
 tree = [[] for _ in range(n + 1)]
+
+# 트리의 간선 정보를 입력받아요
 for _ in range(n - 1):
-    a, b, c = map(int, sys.stdin.readline().split())
-    tree[a].append((b, c))
-max_n = 0
-def dfs(n, d):
-    global max_n
-    left, right = 0, 0
-    for node, w in tree[n]:
-        temp = dfs(node, w)
-        if left <= right:
-            left = max(left, temp)
-        else:
-            right = max(right, temp)
-    max_n = max(max_n, left + right)
-    return max(left + d, right + d)
-dfs(1, 0)
-print(max_n)
+    parent, child, weight = map(int, input().split())
+    tree[parent].append((child, weight))
+    tree[child].append((parent, weight))  # 양방향으로 저장해야 해요
+
+# 첫 번째 DFS: 임의의 노드(여기서는 1번)에서 가장 먼 노드를 찾아요
+def dfs1(node, distance):
+    visited = [False] * (n + 1)
+    stack = [(node, distance)]
+    visited[node] = True
+    max_distance = 0
+    farthest_node = node
+    
+    while stack:
+        current, current_distance = stack.pop()
+        
+        if current_distance > max_distance:
+            max_distance = current_distance
+            farthest_node = current
+        
+        for next_node, weight in tree[current]:
+            if not visited[next_node]:
+                visited[next_node] = True
+                stack.append((next_node, current_distance + weight))
+    
+    return farthest_node, max_distance
+
+# 두 번째 DFS: 첫 번째 DFS에서 찾은 가장 먼 노드에서 다시 가장 먼 노드를 찾아요
+def dfs2(node):
+    visited = [False] * (n + 1)
+    stack = [(node, 0)]
+    visited[node] = True
+    max_distance = 0
+    
+    while stack:
+        current, current_distance = stack.pop()
+        
+        max_distance = max(max_distance, current_distance)
+        
+        for next_node, weight in tree[current]:
+            if not visited[next_node]:
+                visited[next_node] = True
+                stack.append((next_node, current_distance + weight))
+    
+    return max_distance
+
+# 첫 번째 DFS로 가장 먼 노드를 찾아요
+farthest_node, _ = dfs1(1, 0)
+
+# 두 번째 DFS로 트리의 지름을 구해요
+diameter = dfs2(farthest_node)
+
+print(diameter)  # 트리의 지름을 출력해요
