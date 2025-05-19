4136c6da58a94d14af8dab8e7e0c70f909345898
이지수 <113964353+jissssu@users.noreply.github.com>
2025-05-19 18:04:57 +0900

    [Silver IV] Title: 설탕 배달, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,18 +1,14 @@
-def sugar_delivery(N):
-    count = 0
-    
-    # 5의 배수가 될 때까지 3씩 빼면서 시도
-    while N >= 0:
-        if N % 5 == 0:  # 5의 배수이면
-            count += N // 5  # 5로 나눈 몫을 더함
-            print(count)
-            return
-        N -= 3  # 3킬로그램 봉지 하나 추가
-        count += 1
-    
-    # 정확히 나눌 수 없다면 -1 출력
-    print(-1)
+import sys
 
-# 입력 받기
-N = int(input())
-sugar_delivery(N)
+N = int(sys.stdin.readline())
+count = 0
+
+while N >= 0:
+    if N % 5 == 0:
+        count += N // 5
+        print(count)
+        break
+    N -= 3
+    count += 1
+else:
+    print(-1)
