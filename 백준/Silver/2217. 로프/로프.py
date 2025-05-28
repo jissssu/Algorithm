c8db623a011c040ea0762709fce773b59425ad8a
이지수 <113964353+jissssu@users.noreply.github.com>
2025-05-28 23:18:53 +0900

    [Silver IV] Title: 로프, Time: 148 ms, Memory: 112136 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.py
@@ -0,0 +1,11 @@
+n = int(input())
+ropes = [int(input()) for _ in range(n)]
+
+ropes.sort(reverse=True)  
+
+max_weight = 0
+for i in range(n):
+    weight = ropes[i] * (i + 1)
+    max_weight = max(max_weight, weight)
+
+print(max_weight)
