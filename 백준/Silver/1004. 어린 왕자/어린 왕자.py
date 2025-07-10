1e9f55279a9dfd395492528d454c84a1515dc6f8
이지수 <113964353+jissssu@users.noreply.github.com>
2025-07-10 23:48:31 +0900

    [Silver III] Title: 어린 왕자, Time: 140 ms, Memory: 110812 KB -BaekjoonHub

---
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.py
@@ -0,0 +1,15 @@
+def is_inside(x, y, cx, cy, r):
+    return (x - cx) ** 2 + (y - cy) ** 2 < r ** 2
+
+T = int(input())
+for _ in range(T):
+    x1, y1, x2, y2 = map(int, input().split())
+    n = int(input())
+    count = 0
+    for _ in range(n):
+        cx, cy, r = map(int, input().split())
+        start_inside = is_inside(x1, y1, cx, cy, r)
+        end_inside = is_inside(x2, y2, cx, cy, r)
+        if start_inside != end_inside:
+            count += 1
+    print(count)
